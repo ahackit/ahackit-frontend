7051a19a76f93f1f91334dc9f2d42ca573142e23
Austin Hackett <ahackit.gmail.com>
2020-07-25 16:10:48 -0500

    All the work

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
+    <NavBar></NavBar>
     <Nuxt />
   </div>
 </template>
 
+<script>
+import NavBar from '@/components/NavBar'
+export default {
+  components: [NavBar],
+}
+</script>
 <style></style>
